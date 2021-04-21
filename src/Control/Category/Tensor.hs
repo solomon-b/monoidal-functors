50108ddb53583506bc72963db72cebcdb74e8902
Solomon Bothwell <ssbothwell@gmail.com>
2021-04-20 19:11:26 -0700

    moar tensor

--- src/Control/Category/Tensor.hs
+++ src/Control/Category/Tensor.hs
@@ -1,15 +1,17 @@
+{-# LANGUAGE MonoLocalBinds #-}
 module Control.Category.Tensor where
 
 import Prelude hiding (id)
 import Control.Applicative
 import Control.Category (Category, id)
-import Data.Bifunctor
+import Data.Biapplicative
 import Data.Functor.Contravariant
 import Data.Profunctor
 import Data.These
+import Data.Void
 
 class (Category p, Category q) => GBifunctor p q r t | t r -> p q where
-  gbimap :: p a b -> q c d -> r (t a c) (t b d)
+  gbimap :: a `p` b -> c `q` d -> (t a c) `r` (t b d)
 
 instance GBifunctor (->) (->) (->) t => GBifunctor Op Op Op t where
   gbimap :: Op a b -> Op c d -> Op (t a c) (t b d)
@@ -35,16 +37,16 @@ instance GBifunctor (Star Maybe) (Star Maybe) (Star Maybe) These where
       That c -> That <$> g c
       These a c -> liftA2 These (f a) (g c)
 
-grmap :: GBifunctor p q r t => q c d -> r (t a c) (t a d)
+grmap :: GBifunctor p q r t => c `q` d -> (t a c) `r` (t a d)
 grmap = gbimap id
 
-glmap :: GBifunctor p q r t => p a b -> r (t a c) (t b c)
+glmap :: GBifunctor p q r t => a `p` b -> (t a c) `r` (t b c)
 glmap = flip gbimap id
 
-data Iso p a b = Iso { fwd :: p a b, bwd :: p b a }
+data Iso p a b = Iso { fwd :: a `p` b, bwd :: b `p` a }
 
 class (Category p, GBifunctor p p p t) => Associative t p where
-  assoc :: forall a b c. Iso p (t a (t b c)) (t (t a b) c)
+  assoc :: Iso p (t a (t b c)) (t (t a b) c)
 
 instance Associative t (->) => Associative t Op where
   assoc :: Iso Op (t a (t b c)) (t (t a b) c)
@@ -82,5 +84,116 @@ instance Associative These (->) where
     }
 
 class Associative t p => Tensor t i p | t -> i where
-  lunit :: forall a. Iso p (t i a) a
-  runit :: forall a. Iso p (t a i) a
+  lunit :: Iso p (t i a) a
+  runit :: Iso p (t a i) a
+
+instance (Tensor t i (->)) => Tensor t i Op where
+  lunit :: Iso Op (t i a) a
+  lunit = Iso
+    { fwd = Op $ bwd lunit
+    , bwd = Op $ fwd lunit
+    }
+
+  runit :: Iso Op (t a i) a
+  runit = Iso
+    { fwd = Op $ bwd runit
+    , bwd = Op $ fwd runit
+    }
+
+instance (Monad m, Tensor t i (->), Associative t (Star m)) => Tensor t i (Star m) where
+  lunit :: Iso (Star m) (t i a) a
+  lunit = Iso
+    { fwd = (`rmap` id) (fwd lunit)
+    , bwd = (`rmap` id) (bwd lunit)
+    }
+
+  runit = Iso
+    { fwd = (`rmap` id) (fwd runit)
+    , bwd = (`rmap` id) (bwd runit)
+    }
+
+instance Tensor (,) () (->) where
+  lunit :: Iso (->) ((), a) a
+  lunit = Iso
+    { fwd = snd
+    , bwd = bipure ()
+    }
+
+  runit :: Iso (->) (a, ()) a
+  runit = Iso
+    { fwd = fst
+    , bwd = (`bipure` ())
+    }
+
+instance Tensor Either Void (->) where
+  lunit :: Iso (->) (Either Void a) a
+  lunit = Iso
+     { fwd = either absurd id
+     , bwd = pure
+     }
+
+  runit :: Iso (->) (Either a Void) a
+  runit = Iso
+    { fwd = either id absurd
+    , bwd = Left
+    }
+
+instance Tensor These Void (->) where
+  lunit :: Iso (->) (These Void a) a
+  lunit = Iso
+    { fwd = these absurd id (flip const)
+    , bwd = That
+    }
+
+  runit :: Iso (->) (These a Void) a
+  runit = Iso
+    { fwd = these id absurd const
+    , bwd = This
+    }
+
+class Associative t p => Symmetric t p where
+  swap :: (t a b) `p` (t b a)
+
+instance (Symmetric t (->)) => Symmetric t Op where
+  swap :: Op (t a b) (t b a)
+  swap = Op swap
+
+instance (Monad m, Symmetric t (->), Associative t (Star m)) => Symmetric t (Star m) where
+  swap :: Star m (t a b) (t b a)
+  swap = Star $ pure . swap
+
+instance Symmetric (,) (->) where
+  swap :: (a, b) -> (b, a)
+  swap (a, b) = (b, a)
+
+instance Symmetric Either (->) where
+  swap :: Either a b -> Either b a
+  swap = either Right Left
+
+instance Symmetric These (->) where
+  swap :: These a b -> These b a
+  swap = these That This (flip These)
+
+class (Symmetric t p, Tensor t i p) => Cartesian t i p | i -> t, t -> i where
+  diagonal :: a `p` (t a a)
+  terminal :: a `p` i
+
+instance Cartesian (,) () (->) where
+  diagonal :: a -> (a , a)
+  diagonal = dup
+
+  terminal :: a -> ()
+  terminal = const ()
+
+instance Cartesian Either Void Op where
+  diagonal :: Op a (Either a a)
+  diagonal = Op merge
+
+  terminal :: Op a Void
+  terminal = Op absurd
+
+dup :: a -> (a, a)
+dup a = (a, a)
+
+merge :: Either a a -> a
+merge = either id id
