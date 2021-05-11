f3cdd8762f01c48bc75f3a990b77d3526f3d153f
Solomon Bothwell <ssbothwell@gmail.com>
2021-05-10 23:29:35 -0700

    Moar Combinators

--- src/Data/Bifunctor/Monoidal/Specialized.hs
+++ src/Data/Bifunctor/Monoidal/Specialized.hs
@@ -4,11 +4,11 @@ import Prelude hiding ((&&), (||))
 
 import Control.Category.Tensor
 import Data.Bifunctor.Monoidal
+import Data.Functor.Contravariant
 import Data.Profunctor
 
 mux :: Semigroupal (->) (,) (,) (,) p => p a b -> p c d -> p (a, c) (b, d)
 mux = curry combine
-
 
 infixr 5 &&
 
@@ -40,3 +40,41 @@ infixr 5 &|
 
 union :: Profunctor p => Semigroupal (->) (,) Either (,) p => p x a -> p x b -> p x (Either a b)
 union pxa pxb = lmap dup $ pxa &| pxb
+
+divide :: (Profunctor p, Semigroupal (->) (,) Either (,) p) => p a x -> p b x -> p (a, b) x
+divide pxa pxb = rmap merge $ pxa &| pxb
+
+splice :: Semigroupal (->) Either (,) (,) p => p a b -> p c d -> p (Either a c) (b, d)
+splice = curry combine
+
+infix 5 |&
+
+(|&) :: Semigroupal (->) Either (,) (,) p => p a b -> p c d -> p (Either a c) (b, d)
+(|&) = splice
+
+diverge :: Semigroupal (->) Either Either Either p => Either (p a b) (p c d) -> p (Either a c) (Either b d)
+diverge = combine
+
+contramapMaybe :: Profunctor p => Semigroupal (->) Either Either Either p => (a -> Maybe b) -> p b x -> p a x
+contramapMaybe f = dimap (maybe (Right ()) Left . f) merge . ultraleft
+
+zig :: (Profunctor p, Semigroupal (->) (,) t Either p) => Either (p x a) (p x b) -> p x (t a b)
+zig = lmap dup . combine
+
+zag :: (Profunctor p, Semigroupal (->) t Either Either p) => Either (p a x) (p b x) -> p (t a b) x
+zag = rmap merge . combine
+
+ultrafirst :: (Profunctor p, Semigroupal (->) (,) (,) Either p) => p a b -> p (a, x) (b, y)
+ultrafirst = zag . Left . zig . Left
+
+ultrasecond :: (Profunctor p, Semigroupal (->) (,) (,) Either p) => p a b -> p (x, a) (y, b)
+ultrasecond = zag . Right . zig . Right
+
+ultraleft :: (Profunctor p, Semigroupal (->) Either Either Either p) => p a b -> p (Either a x) (Either b y)
+ultraleft = zag . Left . zig . Left
+
+ultraright :: (Profunctor p, Semigroupal (->) Either Either Either p) => p a b -> p (Either x a) (Either y b)
+ultraright = zag . Right . zig . Right
+
+--comux :: forall p a b c d. Semigroupal Op (,) (,) (,) p => p (a, c) (b, d) -> (p a b, p c d)
+--comux = getOp Op (combine @Op @(,) @(,) @(,) @p)
