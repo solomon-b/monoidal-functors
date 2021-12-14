77415c7b525628097fd5fc6699a9ba052ab79d1a
solomon <ssbothwell@gmail.com>
2021-12-13 21:51:37 -0800

    Updates infra and bumps version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,10 @@
 # Revision history for monoidal-functors
 
-## 0.1.0.0 -- YYYY-mm-dd
+## 0.1.1 -- 2021-12-13
 
-* First version. Released on an unsuspecting world.
+* Removes redundant `Iso` types.
+* Some intitial attempts at documentation.
+
+## 0.1.0.0 -- 2021-04-19
+
+* First version.
--- shell.nix
+++ shell.nix
@@ -1,19 +1,22 @@
+{ pkgs ? import <nixpkgs> {} }:
 let
-  sources = import ./nix/sources.nix;
-  pkgs = import sources.nixpkgs { };
+  easy-hls-src = pkgs.fetchFromGitHub {
+    owner  = "jkachmar";
+    repo   = "easy-hls-nix";
+    rev    = "7c123399ef8a67dc0e505d9cf7f2c7f64f1cd847";
+    sha256 = "sha256-/crlHEVB148PGQLZCsHOR9L5qgvCAfRSocIoKgmMAhA=";
+  };
 
-  easy-hls = pkgs.callPackage sources.easy-hls-nix {
-    ghcVersions = [ "8.10.4" ];
+  easy-hls = pkgs.callPackage easy-hls-src {
+    ghcVersions = [ "8.10.7" ];
   };
 
   editorTooling = [
-    pkgs.hlint
     pkgs.cabal-install
-    pkgs.cabal2nix
     easy-hls
   ];
 
-  buildDeps = [ pkgs.haskell.compiler.ghc8104 ];
+  buildDeps = [ pkgs.haskell.compiler.ghc8107 ];
 in
 pkgs.mkShell {
   buildInputs = editorTooling ++ buildDeps;
