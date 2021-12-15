41738e6002a6f793dbb78e40d6347531088a0edc
Dmitry Vagner <dmitry.vagner@holmusk.com>
2021-12-15 11:35:04 -0800

    reorder and cluster

--- src/Control/Category/Tensor.hs
+++ src/Control/Category/Tensor.hs
@@ -14,8 +14,8 @@ import           Prelude                    hiding (id)
 
 | Tensor | Unit |
 +--------|------+
-| Either | Void |
 |  (,)   |  ()  |
+| Either | Void |
 | These  | Void |
 
 tensor (or monoidal category) structure
@@ -31,6 +31,9 @@ operations `t2 (f a) (f b) -> f (t1 a b)` and `i2 -> f i1`
 
 -}
 
+data Iso cat a b = Iso { fwd :: cat a b, bwd :: cat b a }
+
+-- Bifunctors
 class (Category cat1, Category cat2, Category cat3) => GBifunctor cat1 cat2 cat3 t | t cat3 -> cat1 cat2 where
   gbimap :: cat1 a b -> cat2 c d -> cat3 (a `t` c)  (b `t` d)
 
@@ -38,12 +41,15 @@ class (Category cat1, Category cat2, Category cat3) => GBifunctor cat1 cat2 cat3
   (#) :: cat1 a b -> cat2 c d -> cat3 (a `t` c)  (b `t` d)
   (#) = gbimap
 
+
 instance GBifunctor (->) (->) (->) t => GBifunctor Op Op Op t where
   gbimap :: Op a b -> Op c d -> Op (t a c) (t b d)
   gbimap (Op f) (Op g) = Op $ gbimap f g
 
+
 instance Bifunctor t => GBifunctor (->) (->) (->) t where
   gbimap = bimap
+
 
 instance GBifunctor (Star Maybe) (Star Maybe) (Star Maybe) These where
   gbimap :: Star Maybe a b -> Star Maybe c d -> Star Maybe (These a c) (These b d)
@@ -53,30 +59,26 @@ instance GBifunctor (Star Maybe) (Star Maybe) (Star Maybe) These where
       That c    -> That <$> g c
       These a c -> liftA2 These (f a) (g c)
 
+
 grmap :: GBifunctor cat1 cat2 cat3 t => cat2 c d -> cat3 (a `t` c) (a `t` d)
 grmap = (#) id
 
 glmap :: GBifunctor cat1 cat2 cat3 t => cat1 a b -> cat3 (a `t` c) (b `t` c)
 glmap = flip (#) id
 
-data Iso cat a b = Iso { fwd :: cat a b, bwd :: cat b a }
 
+-- Associative Bifunctors
 class (Category cat, GBifunctor cat cat cat t) => Associative cat t where
   assoc :: Iso cat (a `t` (b `t` c)) ((a `t` b) `t` c)
 
+
 instance Associative (->) t => Associative Op t where
-  assoc :: Iso Op (t a (t b c)) (t (t a b) c)
+  assoc :: Iso Op (a `t` (b `t` c)) ((a `t` b) `t` c)
   assoc = Iso
     { fwd = Op $ bwd assoc
     , bwd = Op $ fwd assoc
     }
 
-instance (Monad m, Associative (->) t, GBifunctor (Star m) (Star m) (Star m) t) => Associative (Star m) t where
-  assoc :: Iso (Star m) (t a (t b c)) (t (t a b) c)
-  assoc = Iso
-    { fwd = (`rmap` id) (fwd assoc)
-    , bwd = (`rmap` id) (bwd assoc)
-    }
 
 instance Associative (->) (,) where
   assoc :: Iso (->) (a, (b, c)) ((a, b), c)
@@ -85,12 +87,14 @@ instance Associative (->) (,) where
     , bwd = \((a, b), c) -> (a, (b, c))
     }
 
+
 instance Associative (->) Either where
   assoc :: Iso (->) (Either a (Either b c)) (Either (Either a b) c)
   assoc = Iso
     { fwd = either (Left . Left) (either (Left . Right) Right)
     , bwd = either (fmap Left) (Right . Right)
     }
+
 
 instance Associative (->) These where
   assoc :: Iso (->) (These a (These b c)) (These (These a b) c)
@@ -99,9 +103,20 @@ instance Associative (->) These where
     , bwd = these (grmap This) (That . That) (flip $ grmap . flip These)
     }
 
+
+instance (Monad m, Associative (->) t, GBifunctor (Star m) (Star m) (Star m) t) => Associative (Star m) t where
+  assoc :: Iso (Star m) (a `t` (b `t` c)) ((a `t` b) `t` c)
+  assoc = Iso
+    { fwd = (`rmap` id) (fwd assoc)
+    , bwd = (`rmap` id) (bwd assoc)
+    }
+
+
+-- Associative unital bifunctors
 class Associative cat t => Tensor cat t i | t -> i where
   unitl :: Iso cat (i `t` a) a
   unitr :: Iso cat (a `t` i) a
+
 
 instance (Tensor (->) t i) => Tensor Op t i where
   unitl :: Iso Op (i `t` a) a
@@ -116,18 +131,6 @@ instance (Tensor (->) t i) => Tensor Op t i where
     , bwd = Op $ fwd unitr
     }
 
-instance (Monad m, Tensor (->) t i, Associative (Star m) t) => Tensor (Star m) t i where
-  unitl :: Iso (Star m) (i `t` a) a
-  unitl = Iso
-    { fwd = (`rmap` id) (fwd unitl)
-    , bwd = (`rmap` id) (bwd unitl)
-    }
-
-  unitr :: Iso (Star m) (a `t` i) a
-  unitr = Iso
-    { fwd = (`rmap` id) (fwd unitr)
-    , bwd = (`rmap` id) (bwd unitr)
-    }
 
 instance Tensor (->) (,) () where
   unitl :: Iso (->) ((), a) a
@@ -142,6 +145,7 @@ instance Tensor (->) (,) () where
     , bwd = (`bipure` ())
     }
 
+
 instance Tensor (->) Either Void where
   unitl :: Iso (->) (Either Void a) a
   unitl = Iso
@@ -154,6 +158,7 @@ instance Tensor (->) Either Void where
     { fwd = either id absurd
     , bwd = Left
     }
+
 
 instance Tensor (->) These Void where
   unitl :: Iso (->) (These Void a) a
@@ -168,25 +173,46 @@ instance Tensor (->) These Void where
     , bwd = This
     }
 
+
+instance (Monad m, Tensor (->) t i, Associative (Star m) t) => Tensor (Star m) t i where
+  unitl :: Iso (Star m) (i `t` a) a
+  unitl = Iso
+    { fwd = (`rmap` id) (fwd unitl)
+    , bwd = (`rmap` id) (bwd unitl)
+    }
+
+  unitr :: Iso (Star m) (a `t` i) a
+  unitr = Iso
+    { fwd = (`rmap` id) (fwd unitr)
+    , bwd = (`rmap` id) (bwd unitr)
+    }
+
+
+-- Symmetric bifunctors
 class Associative cat t => Symmetric cat t where
   swap :: cat (a `t` b) (b `t` a)
+
 
 instance (Symmetric (->) t) => Symmetric Op t where
   swap :: Op (a `t` b) (b `t` a)
   swap = Op swap
 
-instance (Monad m, Symmetric (->) t, Associative (Star m) t) => Symmetric (Star m) t where
-  swap :: Star m (a `t` b) (b `t` a)
-  swap = Star $ pure . swap
 
 instance Symmetric (->) (,) where
   swap :: (a, b) -> (b, a)
   swap (a, b) = (b, a)
 
+
 instance Symmetric (->) Either where
   swap :: Either a b -> Either b a
   swap = either Right Left
 
+
 instance Symmetric (->) These where
   swap :: These a b -> These b a
   swap = these That This (flip These)
+
+
+instance (Monad m, Symmetric (->) t, Associative (Star m) t) => Symmetric (Star m) t where
+  swap :: Star m (a `t` b) (b `t` a)
+  swap = Star $ pure . swap
