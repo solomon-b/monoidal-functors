2f98160420dff36b6b45f311f88ad27dc3e61653
solomon <ssbothwell@gmail.com>
2025-08-27 13:03:57 -0700

    Updatex nixpkgs.

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Monoidal Functors";
 
   inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs/nixos-23.11;
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-25.05;
     flake-utils.url = github:numtide/flake-utils;
   };
 
