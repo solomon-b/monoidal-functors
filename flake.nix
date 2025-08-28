86189527680986f5fc862639b5a0552fb18bbf62
solomon <ssbothwell@gmail.com>
2025-08-28 11:12:31 -0700

    plan2json test

--- flake.nix
+++ flake.nix
@@ -4,19 +4,22 @@
   inputs = {
     nixpkgs.url = github:NixOS/nixpkgs/nixos-25.05;
     flake-utils.url = github:numtide/flake-utils;
+    cabalPlan2nix.url = github:solomon-b/cabalPlan2nix;
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, cabalPlan2nix }:
     let
       ghcVersion = "963";
       compiler = "ghc${ghcVersion}";
       overlay = import ./overlay.nix;
-      overlays = [ overlay ];
+      overlays = [overlay cabalPlan2nix.overlays.default];
     in
     flake-utils.lib.eachDefaultSystem
       (system:
         let
           pkgs = import nixpkgs { inherit system overlays; };
+          inherit (pkgs) lib;  # Import lib
+          cabalPlan2nixResult = pkgs.haskell.lib.cabalPlan2nix ./plan.json /home/solomon/Development/Haskell/monoidal-functors;
         in
         rec {
           devShell = pkgs.mkShell {
@@ -32,6 +35,7 @@
           formatter = pkgs.nixpkgs-fmt;
           packages = flake-utils.lib.flattenTree {
             monoidal-functors = pkgs.haskellPackages.monoidal-functors;
+            monoidal-functors-with-deps = (cabalPlan2nixResult.overridePackageSet pkgs.haskellPackages).monoidal-functors;
           };
 
           defaultPackage = packages.monoidal-functors;
